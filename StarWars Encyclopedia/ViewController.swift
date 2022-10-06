939ad4e5d55aa80c4be629d4b4c396736a67f2ad
Ivan Romero <ivan_manuel_romero@hotmail.com>
2022-10-05 22:01:26 -0300

    Added Assets for start proyect

---
+++ StarWars Encyclopedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarWars Encyclopedia
+//
+//  Created by Ivan Romero on 05/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
