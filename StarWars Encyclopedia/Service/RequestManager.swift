5a26d711ee012c167c4dea86352054eed01c1479
Ivan Romero <ivan_manuel_romero@hotmail.com>
2023-03-17 19:49:44 -0300

    Cleaned RequestManager

--- StarWars Encyclopedia/Service/RequestManager.swift
+++ StarWars Encyclopedia/Service/RequestManager.swift
@@ -33,31 +33,6 @@ class RequestManager {
         }.resume()
     }
     
-    func makeRequest<T: Decodable>(url: URL?, type: T.Type, completion: @escaping (Swift.Result<T, Error>) -> Void) {
-        guard let url = url else {
-            completion(.failure(serviceError.urlError))
-            return
-        }
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data else {
-                if let error = error {
-                    completion(.failure(error))
-                } else {
-                    completion(.failure(serviceError.dataError))
-                }
-                return
-            }
-            
-            do {
-                let result = try JSONDecoder().decode(type, from: data)
-                completion(.success(result))
-            } catch let error {
-                completion(.failure(error))
-            }
-        }.resume()
-    }
-    
     func getURL(valueCategoryPath: String, valueIdPath: String? = nil) -> URL?{
         var url = URLComponents()
         url.host = "swapi.dev"
@@ -69,29 +44,6 @@ class RequestManager {
             url.path = "/api/\(valueCategoryPath)/"
         }
         
-        if let url = url.string {
-            print(url)
-            return URL(string: url)
-        } else {
-            print(serviceError.urlError)
-            return nil
-        }
-    }
-    
-    func getGoogleURL(searchString: String) -> URL?{
-        var url = URLComponents()
-        url.host = "www.googleapis.com"
-        url.scheme = "https"
-        
-        url.path = "/customsearch/v1"
-        
-        url.queryItems = [
-        URLQueryItem(name: "q", value: searchString),
-        URLQueryItem(name: "key", value: "AIzaSyCC6X64lwICyjmfLTIRlSRqyPJybE41qcc"),
-        URLQueryItem(name: "cx", value: "b387487f4c73048cc"),
-        ]
-        
-        print(url.string)
         if let url = url.string {
             print(url)
             return URL(string: url)
@@ -113,7 +65,7 @@ class RequestManager {
     }
 }
 
-enum serviceError: Error {
+fileprivate enum serviceError: Error {
     case urlError
     case dataError
 }
